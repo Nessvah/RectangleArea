e761f8e13c0107f223a4a941103ba1e5cf02d068
Nessvh <vanessamarquesns@outlook.pt>
2021-08-11 10:27:22 +0100

    Add Comments

--- rectanglearea.c
+++ rectanglearea.c
@@ -3,16 +3,20 @@
 int main ()
 
 {
+    //Creating the variables
     double width, height, perimeter, area;
 
-    width = 114.5;
-    height = 352.3;
+    width = 74.12;
+    height = 35.53;
 
+    //Formula for perimeter
     perimeter = (height + width) * 2.0;
 
+    //Formula for area
     area = width * height;
 
-    printf("The height is %f\n The width is %f\n The perimeter is %f\n", height, width, perimeter);
-    printf("The height is %f\n The width is %f\n The area is %f\n", height, width, area);
+    //Output with only two decimals values
+    printf("The height is %.2f \n The width is %.2f\n The perimeter is %.2f\n", height, width, perimeter);
+    printf("The height is %.2f\n The width is %.2f\n The area is %.2f\n", height, width, area);
 }
 
